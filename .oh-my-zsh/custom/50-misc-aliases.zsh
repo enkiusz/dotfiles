1bfb96b4221481d7ff32b467ef988d935df7fea8
Maciej Grela <enki@fsck.pl>
2018-12-09 18:16:04 +0100

    oh-my-zsh: Remove burp alias as broken in i3menu, add a more useful variant of lsblk

--- .oh-my-zsh/custom/50-misc-aliases.zsh
+++ .oh-my-zsh/custom/50-misc-aliases.zsh
@@ -1,6 +1,6 @@
 alias sel="xclip -selection clipboard -o"
 alias bkg="time nice -n 19 ionice -c 3" # Useful for running daily emerge
 alias kbugz="bugz --connection kernel"
-alias burp="java -jar $HOME/bin/burpsuite_free_*.jar &"
 alias pulseloop="pactl load-module module-loopback latency_msec=1"
 alias ls="ls -B --color=auto"
+alias lsblk="lsblk -o NAME,RM,SIZE,VENDOR,MODEL,REV,SERIAL,MOUNTPOINT"
